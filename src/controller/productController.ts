31d9ab09431b00b5891e59b9695dc0a499473d7e
Enkh-Amgalan <48120791+enkhmgln@users.noreply.github.com>
2024-03-05 11:24:30 +0800

    functional product service changed to class service

--- src/controller/productController.ts
+++ src/controller/productController.ts
@@ -1,12 +1,12 @@
 import { Request, Response, NextFunction } from "express";
 import asyncHandler from "../middlewares/asyncHandler";
-import * as productService from "../service/product.service";
+import ProductService from "../service/product.service";
 
 class ProductController {
   createProduct = asyncHandler(
     async (req: Request, res: Response, next: NextFunction) => {
       const { name, price } = req.body;
-      const newProduct = await productService.createProduct(name, price);
+      const newProduct = await ProductService.createProduct(name, price);
       res.status(200).json({
         success: true,
         product: newProduct,
@@ -17,36 +17,34 @@ class ProductController {
   );
   getProducts = asyncHandler(
     async (req: Request, res: Response, next: NextFunction) => {
-      const products = await productService.getProductsService();
+      const products = await ProductService.getProductsService();
       res.status(200).json({ success: true, products, error: null });
     }
   );
   getProduct = asyncHandler(
     async (req: Request, res: Response, next: NextFunction) => {
       const { productId } = req.params;
-      const product = await productService.getProductService(productId);
+      const product = await ProductService.getProductService(productId);
       res.status(200).json({ success: true, product, error: null });
     }
   );
   deleteProduct = asyncHandler(
     async (req: Request, res: Response, next: NextFunction) => {
       const { productId } = req.params;
-      const product = await productService.deleteProduct(productId);
-      res
-        .status(200)
-        .json({
-          success: true,
-          product,
-          error: null,
-          message: "`Бүтээгдэхүүнийг амжилттай устгалаа",
-        });
+      const product = await ProductService.deleteProduct(productId);
+      res.status(200).json({
+        success: true,
+        product,
+        error: null,
+        message: "`Бүтээгдэхүүнийг амжилттай устгалаа",
+      });
     }
   );
   updateProduct = asyncHandler(
     async (req: Request, res: Response, next: NextFunction) => {
       const { productId } = req.params;
 
-      const product = await productService.updateProduct(productId, req.body);
+      const product = await ProductService.updateProduct(productId, req.body);
       res.status(200).json({
         success: true,
         product,
